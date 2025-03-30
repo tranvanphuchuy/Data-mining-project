4c7aa4728416c8d6ce2996fbfde85ff7d5c7be06
Van Tran <trabvanphuchuy@gmail.com>
2025-03-29 22:41:07 -0500

    1

--- Image_scraper/patch.py
+++ Image_scraper/patch.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun May 23 14:44:43 2021
-
-@author: Yicong
-"""
 #!/usr/bin/env python3
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
